5481d717be5a643b46960dcf90d6773dd6a587b4
Jason A. Crome <jason@crome-plated.com>
2017-08-28 18:17:37 -0500

    Finished command shell for formula parser.
    I added parsing for command line arguments. You can now pass one or more
    formulas via the command line. If none are passed, the user is dropped
    into an interactive shell, where a formula is entered then immediately
    evaluated.
    
    Everything in the shell is documented now.

--- bin/trak.pl
+++ bin/trak.pl
@@ -1,34 +1,101 @@
 #!/usr/bin/env perl
 
-use lib './lib';
+# Language features
 use v5.24;
 use strictures 2;
+use feature qw(signatures);
+no warnings qw(experimental::signatures);
+
+# Everything else
+use lib './lib';
 use Term::UI;
 use Term::ReadLine;
+use Getopt::Long;
+use Pod::Usage;
 use Trak::Calc;
 
-my $term = Term::ReadLine->new('Trak');
-say qq{
+# Parse CLI options
+GetOptions(
+    'debug|d' => \my $debug,
+    'help|h'  => \my $help,
+) or die pod2usage(2);
+my @formulas = @ARGV;
+
+# Do nothing else if help was asked for
+pod2usage(0) if $help;
+
+# Calculate the formulas given, or drop into interactive mode
+my $calc = Trak::Calc->new;
+if( scalar @formulas > 0 ) {
+    say $calc->calculate($_) foreach @formulas;
+}
+else {
+    do_interactive( $calc );
+}
+
+say "All done! Goodbye.";
+exit 0;
+
+# Give users an interactive session
+sub do_interactive( $calc ) {
+    my $term = Term::ReadLine->new('Trak');
+
+    say qq{
 Hi there! I'm a simple wrapper around Trak::Calc. My purpose in life
 is to make it easy to enter one or more formulas to evaluate into
 the calculator and evaluate the result. If there's a problem with
 your formula, I will tell you that too. So let's get started!
-};
+    };
 
-my $calc = Trak::Calc->new;
-my $input;
-do {
-    $input = $term->get_reply( prompt => "Enter a formula, or 'exit' to finish" );
-    if( $input ne "exit" ) {
-        try {
+    my $input;
+    do {
+        $input = $term->get_reply( prompt => "Enter a formula, or 'exit' to finish" );
+        if( $input ne "exit" ) {
             say $calc->calculate( $input );
         }
-        catch {
-            die $_;
-        };
-    }
-} while( $input ne "exit" );
+    } while( $input ne "exit" );
+}
 
-say "Ok. All done! Goodbye.";
-exit 1;
+__END__
 
+=head1 NAME
+
+bin/trak.pl - Run the IssueTrak formula parser
+
+=head1 SYNOPSIS
+
+    ./bin/trak.pl [--debug] [--help] [formula]
+
+=head1 OPTIONS
+
+=over 4
+
+=item debug
+
+Turn on debugging. This adds a lot of information to the display when the 
+forumla parser is running.
+
+=item help
+
+Show the help information.
+
+=item formula
+
+One or more formulas, wrapped in quotations. One formula per quotation.
+
+=back
+
+=head1 DESCRIPTION
+
+This program tries to parse any formulas passed through the command line 
+directly. If no formulas were given, launch an interactive shell that lets
+the user enter a formula and execute it immediately.
+
+The calculator is implemented by L< Trak::Calc >. See the documentation there
+for specifics on how it is implemented.
+
+=head1 AUTHOR
+
+Jason A. Crome C< jason@crome-plated.com >
+
+=cut
