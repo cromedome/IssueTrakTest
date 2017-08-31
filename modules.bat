5ddc3ae6d63ea548157e5b4909792b7c03b1ca60
Jason A. Crome <jason@crome-plated.com>
2017-08-31 10:31:35 -0500

    cpanm and ppm are batch files, so we must 'call' them.

--- modules.bat
+++ modules.bat
@@ -1,22 +1,21 @@
-cpanm strictures
-cpanm List::MoreUtils::XS
-cpanm Term::UI
-cpanm Math::Trig
-cpanm Moose
-cpanm MooseX::MarkAsMethods
-cpanm MooseX::NonMoose
-cpanm Perl::Critic
-cpanm Perl::Critic::Policy::Miscellanea::RequireRcsKeywords
-cpanm Perl::Critic::Policy::NamingConventions::ProhibitMixedCaseSubs
-cpanm Perl::Critic::Policy::NamingConventions::ProhibitMixedCaseVars
-cpanm Test::More
-cpanm Test::Most
-cpanm Test::Output
-cpanm Perl::Critic
-cpanm Perl::Critic::Policy::Miscellanea::RequireRcsKeywords
-cpanm Perl::Critic::Policy::NamingConventions::ProhibitMixedCaseSubs
-cpanm Perl::Critic::Policy::NamingConventions::ProhibitMixedCaseVars
-cpanm Test::Perl::Critic
-ppm install Scalar::Util::Numeric
-ppm install indirect multidimensional bareword::filehandles
-
+call cpanm strictures
+call cpanm List::MoreUtils::XS
+call cpanm Term::UI
+call cpanm Math::Trig
+call cpanm Moose
+call cpanm MooseX::MarkAsMethods
+call cpanm MooseX::NonMoose
+call cpanm Perl::Critic
+call cpanm Perl::Critic::Policy::Miscellanea::RequireRcsKeywords
+call cpanm Perl::Critic::Policy::NamingConventions::ProhibitMixedCaseSubs
+call cpanm Perl::Critic::Policy::NamingConventions::ProhibitMixedCaseVars
+call cpanm Test::More
+call cpanm Test::Most
+call cpanm Test::Output
+call cpanm Perl::Critic
+call cpanm Perl::Critic::Policy::Miscellanea::RequireRcsKeywords
+call cpanm Perl::Critic::Policy::NamingConventions::ProhibitMixedCaseSubs
+call cpanm Perl::Critic::Policy::NamingConventions::ProhibitMixedCaseVars
+call cpanm Test::Perl::Critic
+call ppm install Scalar::Util::Numeric
+call ppm install indirect multidimensional bareword::filehandles
