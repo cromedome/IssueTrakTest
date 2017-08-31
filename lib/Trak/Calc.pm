6915dd309b1b753d02b55e15b6c772a654b95fed
Jason A. Crome <jason@crome-plated.com>
2017-08-30 21:19:16 -0500

    Bug fixes, enhancements, and changes, oh my!
    Fixed a few hard-to-find calculation errors, changed when we show the
    trace (before the token is put on the stacks), and fixed a bug with how
    iterations are counted. And moved where the report header happens.
    
    And a new method that does calculation (evaluate()), leaving calculate()
    as the entry point for the calculator.

--- lib/Trak/Calc.pm
+++ lib/Trak/Calc.pm
@@ -18,6 +18,10 @@ has debug => (
     isa => 'Bool',
 );
 
+# By making this state, it retains its value through multiple calls of calculate().
+# Only gets reset by evaluate().
+state $iteration = 0;
+
 #
 # Supported operators, their precedence (order), association (dir - L is left-to-right,
 # R is right-to-left), description (help), and implementation.
@@ -55,12 +59,23 @@ my %functions = (
     tan  => { help => "Tangent: tan(x)",          exec => sub { return tan shift;  }}, 
 );
 
-# Evaluate the function given and return the result.
+# Calculate is a front-end for evaluate. It throws up a report header, runs the calculation,
+# and resets the interation counter when done.
 sub calculate ( $self, $formula ) {
     die "No formula provided!\n" unless $formula;
 
+    $self->_log( " #  Token Number Stack          Operator Stack        Action    Remaining" );
+    $self->_log( "--- ----- --------------------- --------------------- --------- ---------------" );
+    $iteration = 1;
+    my $value = $self->evaluate( $formula );
+    $iteration = 0;
+    return $value;
+}
+
+# Evaluate the function given and return the result.
+sub evaluate ( $self, $formula ) {
+    die "No formula provided!\n" unless $formula;
     my( @opstack, @numstack );
-    my $iteration = 1;
 
     # 
     # Set up reporting.
@@ -68,14 +83,7 @@ sub calculate ( $self, $formula ) {
     # This anonymous subroutine allows us to conveniently, repeatedly call this reporting
     # method, and gives us access to all local vars in calculate(). Cool!
     #
-    my $trace = sub ( $action = "", $token = "", $header = 0 ) {
-        # Print trace output header
-        if( $header ) {
-            $self->_log( " #  Token Number Stack          Operator Stack        Action    Remaining" );
-            $self->_log( "--- ----- --------------------- --------------------- --------- ---------------" );
-            return;
-        }
-
+    my $trace = sub ( $action = "", $token = "" ) {
         # Generate a trace message
         $self->_log( sprintf "%-3d %-5s %-21s %-21s %-9s %-15s",
             $iteration,
@@ -87,37 +95,37 @@ sub calculate ( $self, $formula ) {
         );
     };
 
-    $trace->( "", "", 1 ); 
     while( length $formula ) {
         my( $token, $type, $arg ) = $self->_pluck_token( \$formula );
-
+        
         # If it's a number, just dump it on the stack and continue.
         if( $type eq "NUM" ) {
-            push @numstack, $token;
             $trace->( "Reduce", $token );
+            push @numstack, $token;
         }
         elsif( $type eq "OP" ) {
-            # See if this operator has a higher precedence than the one at the top of the
-            # stack. If not, pop the one off the top of the stack, pop two numbers off the 
-            # number stack, evaluate the result, and push the result back on the number stack.
+            # Push the operator on the stack if the stack is empty, or if the precedence is 
+            # greater than to the op on top of the stack.
             if( @opstack == 0 or $ops{ $token }{ order } >= $ops{ $opstack[-1] }{ order }) { 
                 push @opstack, $token;
                 $trace->( "Shift", $token );
             }
             else {
-                my $t2 = pop @numstack;
-                my $op = pop @opstack;
-                my $t1 = pop @numstack;
-                my $result = $ops{ $op }{ exec }->( $t1, $t2 );
-                push @numstack, $result;
+                while( scalar @opstack > 0 and $ops{ $token }{ order } < $ops{ $opstack[-1] }{ order } ) {
+                    my $op = pop @opstack;
+                    my( $t1, $t2 ) = splice @numstack, -2, 2; # Dumb array hack
+                    my $result = $ops{ $op }{ exec }->( $t1, $t2 );
+                    push @numstack, $result;
+                    $trace->( "Evaluate", $result );
+                }
+                $trace->( "Reduce", $token );
                 push @opstack, $token;
-                $trace->( "Evaluate", $result );
             }
         }
         elsif( $type eq "FUNC" ) {
             my $result = $functions{ $token }{ exec }->( $arg );
-            push @numstack, $result;
             $trace->( "Evaluate", $token );
+            push @numstack, $result;
         }
         else {
             die "Unknown token: $token\n";
@@ -126,24 +134,18 @@ sub calculate ( $self, $formula ) {
         ++$iteration;
     }
 
-    my $value = $self->_evaluate;
+    # All done. Empty the stacks and evaluate the result.
+    my $value = pop @numstack;
+    while( @opstack ) {
+        my $op = pop @opstack;
+        my $t1 = pop @numstack;
+        my $result = $ops{ $op }{ exec }->( $t1, $value );
+        $trace->( "Calculate", $result );
+        ++$iteration; # Calculation takes time too!
+        $value = $result;
+    }
+
     return $value;
-}
-
-# TODO: this
-sub _evaluate( $self ) {
-    # All done! Traverse the stacks from the bottom up and calculate the result
-    # TODO: precedence bug when high-precedence operator is at end of formula
-    #my $value = pop @{ $self->_numstack };;
-    #while( @tokens ) {
-        #my $op = shift @tokens;
-        #my $t1 = shift @tokens;
-        #my $result = $ops{ $op }{ exec }->( $value, $t1 );
-        #$self->_trace( "$value $op $t1 = $result" );
-        #$value = $result;
-    #}
-
-    #return $value;
 }
 
 #
@@ -160,14 +162,13 @@ sub _evaluate( $self ) {
 # There is a bit of redundant code here. This was necessited by a bug I found
 # in Perl's regex parser in the formula tokenizer. More below.
 #
-# TODO: whitepsace handling bug
 sub _pluck_token( $self, $formula ) {
     # Ignore whitespace
     $$formula =~ s/^\s+//;
 
     # Positive or negative number
     my( $token, $type, $arg );
-    if( $$formula =~ /^([+-]?\d+\.?\d*?)(.*)$/x ) {
+    if( $$formula =~ /^([-]?\d+\.?\d*?)(.*)$/x ) {
         $token    = $1;
         die "Parse error: $1 isn't an integer!\n" unless isint( $token );
         $type     = "NUM";
@@ -185,7 +186,6 @@ sub _pluck_token( $self, $formula ) {
         $type     = "FUNC";
         $$formula = $2;
         $functions{ $token } or die "Unknown function: '$token'\n";
-        #$self->_trace( "Found function $token" );
 
         # Get the argument. The second regex *should* have done this, but there
         # is a bug in the Perl regex engine that is causing the second capture to 
@@ -194,11 +194,10 @@ sub _pluck_token( $self, $formula ) {
         # to work around this with a simple search-and-replace, but in doing so, I
         # had to make a little redundant code in the operator and number blocks. :(
         $$formula =~ s/^\s+//;
-        $$formula =~ /^([+-]?\d+)\s*?\)([^\)]*)$/;
+        $$formula =~ /^([-]?\d+)\s*?\)([^\)]*)$/;
         die "Parse error: ')' expected\n" unless $1;
         $arg = $1;
         $$formula =~ s/$2//g;
-        #$self->_trace( "Found argument '$arg'" );
     }
     elsif( $$formula =~ /^(\()(.*)$/ ) {
         # Handle parenthetical expressions.
@@ -210,13 +209,11 @@ sub _pluck_token( $self, $formula ) {
         $$formula =~ /^(.*?)\)(.*)$/;
         die "Parse error: ')' expected\n" unless $1;
 
-        # Pass only the parenthetical piece. Make sure we remove it from the original
-        # formula.
-        my $f2 = $1;
-        $$formula =~ s/^(.*?)\)//g;
-        #$self->_trace( "Nested formula is $f2, remaining is $$formula" );
-        $token = $self->calculate( $f2 );
+        # Pass only the parenthetical piece. Make sure we remove it from the original formula.
+        my $f2 = $1; $f2 =~ s/^\s+//; $f2 =~ s/\s+$//;
+        $token = $self->evaluate( $f2 );
         $type = "NUM";
+        $$formula =~ s/^.*?\)//g;
     }
     else {
         $token = $$formula;
